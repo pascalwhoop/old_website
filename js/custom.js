e41e59ca78de908545fb2c3395f6188b7d89b2c9
Pascal Brokmeier <pascal.brokmeier@gmail.com>
2016-01-25 15:50:49 +0100

    loading animation, added about, fixed disqus hopefully

--- js/custom.js
+++ js/custom.js
@@ -3,32 +3,41 @@
 var blocked = false;
 
 function switchLoadingDisplay() {
-      // loader page
-      var page = document.getElementById("page");
-      var loader = document.getElementById("loader");
-      loader.style.display = "none";
-      document.getElementsByClassName("mdl-layout__content")[0].style.width="100%";
-      page.style.display = "block";
+    // loader page
+    var page = document.getElementById("page");
+    var loader = document.getElementById("loader");
+    loader.style.display = "none";
+    document.getElementsByClassName("mdl-layout__content")[0].style.width = "100%";
+    page.style.display = "block";
 
 }
-var onLoad = function(){
-      switchLoadingDisplay();
 
-      var mainScrollArea = document.getElementsByClassName('mdl-layout__content')[0];
-      if(window.location.href == localStorage.getItem('lastUrl')) {
-                  mainScrollArea.scrollTop = localStorage.getItem('scrollTop');
-            } else {
-                  localStorage.setItem('lastUrl', window.location.href);
-                  localStorage.setItem('scrollTop', 0);
-            }
 
-      mainScrollArea.addEventListener('scroll', function() {
-            if(!blocked){
-                  localStorage.setItem('scrollTop', this.scrollTop);
-                  blocked = true;
-                  setTimeout(function(){blocked = false;}, 200);    
-            } 
-      });
-}
+var onLoad = function () {
+    switchLoadingDisplay();
+
+    var mainScrollArea = document.getElementsByClassName('mdl-layout')[0];
+    setTimeout(function () {
+        if (window.location.href == localStorage.getItem('lastUrl')) {
+            mainScrollArea.scrollTop = localStorage.getItem('scrollTop');
+        } else {
+            localStorage.setItem('lastUrl', window.location.href);
+            localStorage.setItem('scrollTop', 0);
+        }
+    }, 100);
+
+
+    mainScrollArea.addEventListener('scroll', function () {
+        if (!blocked) {
+            localStorage.setItem('scrollTop', this.scrollTop);
+            blocked = true;
+            setTimeout(function () {
+                blocked = false;
+            }, 200);
+        }
+    });
+
+};
 
 window.onload = onLoad;
+
